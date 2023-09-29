b24111dffdd25c96e076a78d466825a12e9d4785
R0ronoaZ0r0 <hemanthredi@gmail.com>
2023-09-28 23:22:42 -0500

    completed CardContent component structure

--- src/Components/CardContent.jsx
+++ src/Components/CardContent.jsx
@@ -1,24 +1,31 @@
 
 // if question is true display question else display answer
+import PropTypes from 'prop-types';
+
 const CardContent = ({index, question}) => {
 
-    const content = [
+        const content = [
 
-        {'question1': 'Answer1'},
-        {'question2': 'Answer2'},
-        {'question3': 'Answer3'}
+                {'question1': 'Answer1'},
+                {'question2': 'Answer2'},
+                {'question3': 'Answer3'}
 
-    ];
+        ];
 
-    return (
-        <div className="card-content">
-          {question ? (
-            <div className="question">{Object.keys(content[index])[0]}</div>
-          ) : (
-            <div className="answer">{Object.values(content[index])[0]}</div>
-          )}
-        </div>
-      );
+        return (
+                <div className="card-content">
+                    {question ? (
+                        <div className="question">{Object.keys(content[index])[0]}</div>
+                    ) : (
+                        <div className="answer">{Object.values(content[index])[0]}</div>
+                    )}
+                </div>
+            );
+};
+
+CardContent.propTypes = {
+    index: PropTypes.number.isRequired,
+    question: PropTypes.bool.isRequired
 };
 
 export default CardContent;
